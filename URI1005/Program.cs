efa5d4c0129cb78dbb8f38f32458f290e85d4b4a
luiz.caetano <luiz.caetano@MMQ003477>
2023-09-22 11:30:16 -0300

    Adicionar arquivos de projeto.

---
+++ URI1005/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class URI1005
+    {
+        static void Main(string[] args)
+        {
+            double A, B, media;
+
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            media = ((A * 3.5) + (B * 7.5)) / 11;
+
+            Console.WriteLine("MEDIA = " + media.ToString ("F5", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+
+        }
+    }
+}
